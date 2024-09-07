26b811e32cf5c16662be7a38096ec156cbffc87a
MaxxPC <maxkanz1991@gmail.com>
2024-09-07 22:07:00 +0400

    added ""Hello QA.GURU!!!!!!!!!!!!!!!!!!!!!!!!""

--- src/main/java/com/Maxkanzas/Main.java
+++ src/main/java/com/Maxkanzas/Main.java
@@ -1,7 +1,7 @@
 package com.Maxkanzas;
 
 public class Main {
-    private static String msg = "Hello QA.GURU!!!!!!";
+    private static String msg = "Hello QA.GURU!!!!!!!!!!!!!!!!!!!!!!!!";
     public static void main(String[] args) {
         System.out.printf(getMessage());
     }
