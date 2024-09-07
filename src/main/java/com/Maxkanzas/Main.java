8643940a6be65f8221da042acc7cf0f5520ce747
MaxxPC <maxkanz1991@gmail.com>
2024-09-07 20:18:35 +0400

    message was changed

--- src/main/java/com/Maxkanzas/Main.java
+++ src/main/java/com/Maxkanzas/Main.java
@@ -2,6 +2,6 @@ package com.Maxkanzas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.printf("Hello QAgit!");
     }
 }
